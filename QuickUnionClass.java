85b399923ff03e38c2f0b6f12e3e56a4b8dcc82a
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2018-12-19 12:01:56 +0000

    Update QuickUnionClass.java
    Improved optimisation by adding path compression and weighted tree analysis, fixed insidious logic bug.

--- QuickUnionClass.java
+++ QuickUnionClass.java
@@ -1,39 +1,65 @@
-public class QuickUnion {
+import java.util.*;
 
-  private int[] values = {0,1,2,3,4,5,6,7,8};
-
-  public static void main(String[] args) {
-
-    QuickUnion test = new QuickUnion();{
-
-  }
-
-  public boolean connected(int val1, int val2){
-
-    return (get_root(val1) == get_root(val2));
-
-  }
-  
-  public int get_root(int val){
-
-    while (val != values[val]){
-
-      val = values[val];
-    }
-
-    return val;
-  }
-
-  public void union (int val1, int val2){
-
-    values[get_root(val1)] = get_root(val2);
+public class QuickUnion{
     
-  }
+    private int[] values;
+    private int[] treeSize;
 
+     public static void main(String []args){
+     }
+     
+     public void create(int size){
+         
+         // declare size trees 
+         
+         values = new int[size];
+         treeSize = new int[size];
+         
+         for (int count=0; count<size; count++){
+             values[count] = count;
+             treeSize[count] = 1;
+         }
+     }
+       
+     public boolean connected(int val1, int val2){
+         
+         return (values[val1] == values[val2]);
+         
+     }
+     
+     public int getRoot(int val){
+         
+         while (val != values[val]){
+             
+             // simple one pass path compression in a single line
+             values[val] = values[values[val]];
+             
+             
+             val = values[val];
+         }
+         
+         return val;
+     }
+     
+     public void union(int val1, int val2){
+         
+         int rootval1 = getRoot(val1);
+         int rootval2 = getRoot(val2);
+         
+         // added weighted tree analysis to get lg N max depth
+         // return call exits method
+         if (rootval1 == rootval2) return;
+          
+         // adding val 1 to val 2, so val 2 tree increases NOT val 1's tree
+         else if (treeSize[rootval1] <= treeSize[rootval2]){
+             values[rootval1] = values[rootval2];
+             treeSize[rootval2] += treeSize[rootval1];
+         }
+         else{
+             values[rootval2] = values[rootval1];
+             treeSize[rootval1] += treeSize[rootval2];
+         }
+         
+     }
+     
 }
-   
-/*    System.out.println(test.connected(3, 2));
-    test.union(3, 2);
-    test.union(4, 5);
-    test.union(3, 4);
-    System.out.println(test.connected(3, 5)); */
